ac636103946a8360f58e7524879632379c1dc2ce
João Pedro <jpssam6@gmail.com>
2024-01-04 16:57:48 -0400

    atualiza aí

--- cypress-basico-v2/cypress/e2e/CAC-TAT.cy.js
+++ cypress-basico-v2/cypress/e2e/CAC-TAT.cy.js
@@ -1,9 +1,22 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-  it('verifica o título da aplicação', function() {
+  beforeEach('Preparação para os testes', () => {
     cy.visit('./src/index.html')
-    
+  })
+  
+  it('Verifica o título da aplicação', () => {
     cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+  })
+
+  it('Preenche os campos obrigatórios e clica no botão Enviar', () => {
+    const LongText = "Teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste"
+    cy.get('#firstName').type('Augusto').should('have.value', 'Augusto')
+    cy.get('#lastName').type('dos Anjos').should('have.value', 'dos Anjos')
+    cy.get('#email').type('augusto@aug.au').should('have.value', 'augusto@aug.au')
+    cy.get('#open-text-area').type(LongText, { delay: 0 }).should('have.value', LongText)
+
+    cy.get('.button').click()
+    cy.get('.success').should('be.visible')
   })
 })
