6d329a2ce2a55b76df07655284768839f40ba099
João Pedro <jpssam6@gmail.com>
2024-01-19 12:10:14 -0400

    e

--- cypress-basico-v2/cypress/e2e/privacy.spec.js
+++
@@ -1,6 +0,0 @@
-it('testa a página da política de privacidade de forma independente', () => {
-    cy.visit('./sc/privacy.html')
-
-    cy.contains('Talkin About Testing')
-    .should('be.visibile')
-  })
